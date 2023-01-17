89869851416ea5f21441d009866544c77763ed05
Baejb <bjb6478@naver.com>
2023-01-17 21:24:41 +0900

    ex002

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { React } from 'react';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>HTML 적용하기</p>
+      <p>css 적용하기</p>
     </div>
   );
 }
